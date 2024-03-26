b34cefdaa137f3ed08f4670531754bf206ece121
Maselecybernova <164945452+Maselecybernova@users.noreply.github.com>
2024-03-26 22:33:52 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,91 @@
+
+
+/*******************************************************************************
+
+Date And Time
+
+*******************************************************************************/
+
+/*
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+  var datetimeElement = document.getElementById("dateTime");
+
+  function updateDateTime() {
+  
+    var now = new Date();
+
+    var dateTimeString = now.toLocaleString();
+
+    datetimeElement.textContent = dateTimeString+" Today";
+  }
+
+  updateDateTime();
+  setInterval(updateDateTime, 1000);
+
+}); */
+
+
+//***************************************************************************//
+
+/*
+function updateNumber() {
+  
+  
+       var randomNum = Math.floor(Math.random() * (80 - 50 + 1) + 50);
+
+       var numberElem = document.getElementById("number");
+
+       numberElem.textContent = "Balance: "+"00." + "00"+ " USD";
+}
+
+setInterval(updateNumber, 2000);  */
+
+/******************************************************************************/
+
+
+
+
+
+function charts(){
+     window.location.href=("Chart.html");
+}
+function Dash(){
+    window.location.href=("------MaseleMarkets0.html");
+}
+function Cashier(){
+    window.location.href=("Cashier.html");
+}
+function Alerts(){
+    alert("You do not have any Notifications");
+}
+function Signals(){
+    window.location.href=("Signals.html");
+}
+function Support(){
+    alert("For more information Contact Sdódó Gale Masele[ gmasele15@gmail.com | WhatsApp 72141974 | Discord gmasele_15]");
+}
+
+
+
+
+
+
+function withdraw(){
+    alert("Your account has no Funds");
+}
+function checkout(){
+    alert("Transaction Failed! Please try Again");
+}
+
+
+
+
+
+
+
+
+
+
+        
