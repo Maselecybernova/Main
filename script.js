2f879eb214b52286f8419b50e1d83edfa4f3404d
Maselecybernova <164945452+Maselecybernova@users.noreply.github.com>
2024-04-11 22:40:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ if (email=="kgakololosupang54@gmail.com"){
 
           var numberElem = document.getElementById("number");
 
-          numberElem.innerHTML = "Balance: " + "<span style='color: #ffffff; font-weight: bold; font-size: 17px;'> $234." + randomNum + "</span> USD";
+          numberElem.innerHTML = "Balance: " + "<span style='color: #ffffff; font-weight: bold; font-size: 17px;'> $23." + randomNum + "</span> USD";
      }
 
 
